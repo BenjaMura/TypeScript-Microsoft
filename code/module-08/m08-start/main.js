5f1e1e2008123cb1d2f0e70d85de7c1ae01dec08
BenjaMura <benjaminmuratore1@gmail.com>
2023-07-03 11:14:45 -0300

    Module 08

--- code/module-08/m08-start/main.js
+++ code/module-08/m08-start/main.js
@@ -1,6 +1,29 @@
+/// <reference path="module08_loans.ts" />
+var LoanPrograms;
+(function (LoanPrograms) {
+    function calculateInterestOnlyLoanPayment(loanTerms) {
+        var payment;
+        payment =
+            loanTerms.principle * calculateInterestRate(loanTerms.interestRate);
+        return "The interest only loan payment is " + payment.toFixed(2);
+    }
+    LoanPrograms.calculateInterestOnlyLoanPayment = calculateInterestOnlyLoanPayment;
+    function calculateConventionalLoanPayment(loanTerms) {
+        var interest = calculateInterestRate(loanTerms.interestRate);
+        var payment;
+        payment =
+            (loanTerms.principle * interest) /
+                (1 - Math.pow(1 / (1 + interest), loanTerms.numberOfMonths));
+        return "The conventional loan payment is " + payment.toFixed(2);
+    }
+    LoanPrograms.calculateConventionalLoanPayment = calculateConventionalLoanPayment;
+    function calculateInterestRate(interestRate) {
+        var interest = interestRate / 1200;
+        return interest;
+    }
+})(LoanPrograms || (LoanPrograms = {}));
 /*  Module 8: Organize code using TypeScript namespaces
     Lab Start */
-
 /*  TODO Create a the Loans namespace. */
 /* namespace Loans {
     export interface Loan {
@@ -11,7 +34,6 @@
         numberOfMonths: number
     }
 } */
-
 // interface Loan {
 //     principle: number,
 //     interestRate: number        //* Interest rate percentage (eg. 14 is 14%)
@@ -19,10 +41,8 @@
 // interface ConventionalLoan extends Loan {
 //     months: number      //* Total number of months
 // }
-
 /*  TODO Create LoanPrograms namespace. */
 // namespace LoanPrograms {
-
 // /*  TODO Update the calculateInterestOnlyLoanPayment function. */
 // export function calculateInterestOnlyLoanPayment(loanTerms: Loans.Loan): string {
 //     let payment: number;
@@ -41,13 +61,11 @@
 //     return interest
 // }
 // }
-
 /*  TODO Add reference paths. */
 /// <reference path="module08_loans.ts" />
 /// <reference path="module08_loan-programs.ts" />
 /*  TODO Update the function calls. */
-
-let interestOnlyPayment = LoanPrograms.calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
-let conventionalLoanPayment = LoanPrograms.calculateConventionalLoanPayment({principle: 30000, interestRate: 5, numberOfMonths: 180});
-console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
-console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
+var interestOnlyPayment = LoanPrograms.calculateInterestOnlyLoanPayment({ principle: 30000, interestRate: 5 });
+var conventionalLoanPayment = LoanPrograms.calculateConventionalLoanPayment({ principle: 30000, interestRate: 5, numberOfMonths: 180 });
+console.log(interestOnlyPayment); //* Returns "The interest only loan payment is 125.00" 
+console.log(conventionalLoanPayment); //* Returns "The conventional loan payment is 237.24"
